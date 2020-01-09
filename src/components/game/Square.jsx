5a4c59ad569322f5c1be1e33ba2f7ff0d61c0ee8
Nicklas766 <Nicklas766@live.se>
2020-01-09 10:37:07 +0100

    made design more responsive and added aria-label to button for accessibility

--- src/components/game/Square.jsx
+++ src/components/game/Square.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Square = ({ onClick, value = '' }) => (
-  <button type="submit" onClick={onClick} className="square">
+  <button aria-label="make game move" type="submit" onClick={onClick} className="square">
     {value}
   </button>
 );
